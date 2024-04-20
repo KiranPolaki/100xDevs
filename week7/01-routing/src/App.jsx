cedbb66d8626baf47626faba095282b206feb479
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-04-20 11:06:13 +0530

    basic routing implemented

---
+++ week7/01-routing/src/App.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Landing } from "./pages/Landing";
+import { Dashboard } from "./pages/Dashboard";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
