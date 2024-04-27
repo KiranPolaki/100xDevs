ad798af39481b4861482e54ee78058ffcddc076d
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-04-27 11:47:09 +0530

    second component completed

--- week7/Assignments/Problems/src/App.jsx
+++ week7/Assignments/Problems/src/App.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
 import { Component1 } from "./components/Component1";
+import { Component2 } from "./components/Component2";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="app-container">
       <Component1 name={"Sai"} age={"21"} place={"Hyderabad"} />
-    </>
+      <Component2 />
+    </div>
   );
 }
 
---
+++ week7/Assignments/Problems/src/components/Component2.jsx
@@ -0,0 +1,67 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+
+export function Component2() {
+  const [gradient, seGradient] = useState([
+    {
+      id: 1,
+      background: "rgb(2,0,36)",
+      gradient:
+        "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)",
+    },
+    {
+      id: 2,
+      background: "rgb(34, 193, 195)",
+      gradient:
+        "linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)",
+    },
+    {
+      id: 3,
+      background: "rgb(131,58,180)",
+      gradient:
+        "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)",
+    },
+    {
+      id: 4,
+      background: "rgb(238,174,202)",
+      gradient:
+        "radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)",
+    },
+    {
+      id: 5,
+      background: "#08AEEA",
+      gradient: "linear-gradient(0deg, #08AEEA 0%, #2AF598 100%)",
+    },
+  ]);
+
+  function handleGradientClick(gradient) {
+    document.documentElement.style.background = gradient.gradient;
+  }
+
+  return (
+    <>
+      <div className="comp2-container">
+        <p className="color">color pick any</p>
+        <div className="buttons-cont">
+          {gradient.map((gradient) => (
+            <button
+              className="button-color"
+              key={gradient.id}
+              style={{
+                background: gradient.gradient,
+                color: "white",
+                border: "none",
+                padding: "10px",
+                margin: "5px",
+                cursor: "pointer",
+              }}
+              onClick={() => {
+                handleGradientClick(gradient);
+              }}
+            ></button>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
