390e4ba4931842317a2e3cb9052f2ca27a844a04
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-05-05 15:59:59 +0530

    data fetch with interval custom hook

--- week9/01-CustomHooks/src/App.jsx
+++ week9/01-CustomHooks/src/App.jsx
@@ -17,16 +17,37 @@ function useTodos() {
   return { todos, loading };
 }
 
+function useTodosInterval(n) {
+  const [todos2, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const value = setInterval(() => {
+      axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
+        setTodos(res.data.todos);
+        setLoading(false);
+      });
+    }, n * 1000);
+
+    return () => {
+      clearInterval(value);
+    };
+  }, [n]);
+
+  return { todos2, loading };
+}
+
 function App() {
   const { todos, loading } = useTodos();
+  const { todos2, loading2 } = useTodosInterval(100);
 
-  if (loading) {
+  if (loading2) {
     return <div>Loading...</div>;
   }
 
   return (
     <>
-      {todos.map((todo) => (
+      {todos2.map((todo) => (
         <Track key={todo.id} todo={todo} />
       ))}
     </>
