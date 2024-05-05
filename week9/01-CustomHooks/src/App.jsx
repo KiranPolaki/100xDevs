015cc9cc0713ad531ea346f8cbfdb040390c8616
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-05-05 11:09:52 +0530

    class based components

--- week9/01-CustomHooks/src/App.jsx
+++ week9/01-CustomHooks/src/App.jsx
@@ -1,7 +1,42 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 import "./App.css";
+import { Component } from "react";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <MyComponent />
+    </>
+  );
+}
+
+// function MyComponent() {
+//   const [count, setCount] = useState(0);
+//   return (
+//     <>
+//       <p>{count}</p>
+//       <button onClick={() => setCount((count) => count + 1)}>+</button>
+//     </>
+//   );
+// }
+
+class MyComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+  incrementCount = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  render() {
+    return (
+      <>
+        <p>{this.state.count}</p>
+        <button onClick={this.incrementCount}>+</button>
+      </>
+    );
+  }
 }
 
 export default App;
