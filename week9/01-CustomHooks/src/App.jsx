4a2667a1e767dfaf667a800d166f5d04ee3f7a46
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-05-05 11:39:55 +0530

    life cycle events created

--- week9/01-CustomHooks/src/App.jsx
+++ week9/01-CustomHooks/src/App.jsx
@@ -1,27 +1,44 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { Component } from "react";
 
+// function App() {
+//   const [render, setRender] = useState(true);
+//   useEffect(() => {
+//     setTimeout(() => {
+//       setRender(!render);
+//     }, 10000);
+//   });
+//   return <>{render ? <MyComponent /> : <>Oops</>}</>;
+// }
+
 function App() {
+  return <MyComponent2 />;
+}
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("COmponent Mounted: When the component get into the picture");
+    return () => {
+      console.log(
+        "Component Unmounter: When the dep array changes or component is replaced then this will run once and changes for the new one"
+      );
+    };
+  }, []);
+
   return (
     <>
-      <MyComponent />
+      {/* <p>{count}</p>
+      <button onClick={() => setCount((count) => count + 1)}>+</button> */}
+      inside from my Component
+      {console.log("inside from my Component")}
     </>
   );
 }
 
-// function MyComponent() {
-//   const [count, setCount] = useState(0);
-//   return (
-//     <>
-//       <p>{count}</p>
-//       <button onClick={() => setCount((count) => count + 1)}>+</button>
-//     </>
-//   );
-// }
-
-class MyComponent extends Component {
+class MyComponent1 extends Component {
   constructor(props) {
     super(props);
     this.state = { count: 0 };
@@ -36,6 +53,20 @@ class MyComponent extends Component {
         <button onClick={this.incrementCount}>+</button>
       </>
     );
+  }
+}
+
+class MyComponent2 extends Component {
+  componentDidMount() {
+    console.log("Component Mounted");
+  }
+
+  componentWillUnmount() {
+    console.log("unmount");
+  }
+
+  render() {
+    return <h1>Hi re</h1>;
   }
 }
 
