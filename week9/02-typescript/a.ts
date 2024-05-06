e7244164756997f903e337cbcb6d09aeee4e891f
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-05-06 22:27:47 +0530

    typescript

--- week9/02-typescript/a.ts
+++
@@ -1,9 +0,0 @@
-function great(firstname: string): string {
-  console.log(`Hello ${firstname}`);
-  return "sai";
-}
-
-great("polaki");
-
-const x: number = 1;
-console.log(x);
---
+++ week9/02-typescript/src/a.ts
@@ -0,0 +1,65 @@
+// * Type Inference - we dont have to mention types of return types sometypes
+function great(firstname: string): string {
+  console.log(`Hello ${firstname}`);
+  return "sai";
+}
+
+function isLegeal(age: number): boolean {
+  if (age > 18) {
+    return true;
+  }
+  return false;
+}
+
+function runAfter1S(fn: () => void) {
+  setTimeout(fn, 1000);
+}
+
+runAfter1S(function () {
+  console.log("Hi");
+});
+
+great("polaki");
+console.log(isLegeal(12) ? "Can Vote" : "Cant vote");
+
+const x: number = 1;
+console.log(x);
+
+// * without Interfaces
+// function isLeagalAgeI(user: {
+//   firstName: string;
+//   lastName: string;
+//   age: number;
+// }): boolean {
+//   if (user.age > 18) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// isLeagalAgeI({
+//   firstName: "Sai",
+//   lastName: "kiran",
+//   age: 21,
+// });
+// * With Interface
+interface User {
+  firstName: string;
+  lastName: string;
+  age: number;
+  email?: string; // optional argument
+}
+
+function isLeagalAgeI(user: User): boolean {
+  if (user.age > 18) {
+    return true;
+  }
+  return false;
+}
+
+isLeagalAgeI({
+  firstName: "Sai",
+  lastName: "kiran",
+  age: 21,
+});
