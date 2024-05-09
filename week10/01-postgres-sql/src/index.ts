c096727f3541d5382fc6daa6c19637ba072271ce
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-05-09 23:58:14 +0530

    update

--- week10/01-postgres-sql/src/index.ts
+++ week10/01-postgres-sql/src/index.ts
@@ -1,11 +1,22 @@
+// write a function to create a users table in your database.
 import { Client } from "pg";
 
 const client = new Client({
-  host: "postgresql://polakisaikiran1:Da4ZxwbvklM0@ep-square-leaf-a5tbp99z.us-east-2.aws.neon.tech/TestAmatarasu?sslmode=require",
-  port: 5534,
-  database: "TestAmatarasu",
-  user: "polakisaikiran1",
-  password: "Da4ZxwbvklM0",
+  connectionString: "postgresql://postgres:mysecretpassword@localhost/postgres",
 });
 
-client.connect();
+async function createUsersTable() {
+  await client.connect();
+  const result = await client.query(`
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            username VARCHAR(50) UNIQUE NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+        );
+    `);
+  console.log(result);
+}
+
+createUsersTable();
